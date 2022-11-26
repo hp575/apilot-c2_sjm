1e75c0836c3a822ee0425d4bfee3a20ab6464702
ajouatom <ajouatom@gmail.com>
2022-11-26 11:45:49 +0900

    battery chargin control

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -213,7 +213,7 @@ def thermald_thread(end_event, hw_queue):
   hotspot_run = False
 
   hotspot_on_boot = False #ntune_option_enabled('OpkrHotspotOnBoot')
-  battery_charging_control = False #ntune_option_enabled('batteryChargingControl')
+  battery_charging_control = True #ntune_option_enabled('batteryChargingControl')
   battery_charging_min = 70 #int(ntune_option_get('batteryChargingMin'))
   battery_charging_max = 80 #int(ntune_option_get('batteryChargingMax'))
 
